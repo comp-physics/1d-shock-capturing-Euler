d51c835d8e5f3b4b0670caadcf70297ae2a30d2b
Spencer Bryngelson <sbryngelson@gmail.com>
2024-06-24 18:54:56 -0400

    add shu osher

--- battery.sh
+++ battery.sh
@@ -1,5 +1,5 @@
 T=3
-L=10
+L=1
 CFLs=( 0.45 )
 
 Ns=( 512 1024 2000 4000 8000 16000 32000 48000 )
@@ -8,13 +8,9 @@ Ns=( 512 1024 2000 4000 8000 16000 32000 48000 )
 amp=3
 Beta=0.95 #for artifical viscosity
 
-enable_adjoint='True' #{True,False}
 enable_viscosity='True' #{True,False}
 ForTime=RK3
-BackTime=RK3
-
 forflux=GLF     #{LLF,GLF,AV}
-backflux=GLF    #{GLF,AV}
 
 rm -rf ./out/* ./O/*
 
@@ -22,12 +18,12 @@ for N in "${Ns[@]}"; do
 for CFL in "${CFLs[@]}"; do
     case='weno-'$T'T-'$N'N-'$CFL'CFL'
     echo Case $case
-    cat ./Input/weno.temp   | sed -e s/XXN/$N/ -e s/XXBETA/$Beta/ -e s/XXCFL/$CFL/ -e s/XXT/$T/ -e s/XXAMP/$amp/ -e s/XXL/$L/ -e s/XXFT/$ForTime/ -e s/XXBT/$BackTime/ -e s/XXFFLUX/$forflux/ -e s/XXAFLUX/$backflux/ -e s/XXADJ/$enable_adjoint/ -e s/XXVISC/$enable_viscosity/ > ./Input/weno.in
+    cat ./Input/weno.temp   | sed -e s/XXN/$N/ -e s/XXBETA/$Beta/ -e s/XXCFL/$CFL/ -e s/XXT/$T/ -e s/XXAMP/$amp/ -e s/XXL/$L/ -e s/XXFT/$ForTime/ -e s/XXFFLUX/$forflux/ -e s/XXVISC/$enable_viscosity/ > ./Input/weno.in
     rm -rf ./D/* 
     #./weno3.x  > './out/'$case'_3.out'
-    ./weno5.x  > './out/'$case'_5.out'
+    ./euler  > './out/'$case'_5.out'
 
-    cp ./D/final* './O/adjfin_'$case'.csv'
+    cp ./D/final* './O/fin_'$case'.csv'
     #cat ./D/l1err.out >> ./O/l1err.out
     #mv ./D/l1err.out './O/l1err_'$case'.dat'
 done
--- ic.f90
+++ ic.f90
@@ -13,10 +13,9 @@ subroutine getic(x,uc,up,ucsol)
     forall(i=0:n:1) x(i) = i*h
 
     x(:) = x(:) - L/2.
-
     do i=0,n
-
         !! Gaussian pulse
+        ! x(:) = x(:) - L/2.
         ! up(1,i) = amp * ( exp( -(x(i)**2.) ) + 1 )
         ! up(2,:) = 1; up(3,:) = 1./g
 
@@ -27,15 +26,27 @@ subroutine getic(x,uc,up,ucsol)
         !     up(1,i) = 2.*amp
         ! end if
 
-        ! Sod Shock tube
-        if (x(i) > 0 ) then
-            up(1,i) = 0.125
-            up(2,i) = 0.000
-            up(3,i) = 0.100
+        !! Sod Shock tube
+        ! if (x(i) > 0 ) then
+        !     up(1,i) = 0.125
+        !     up(2,i) = 0.000
+        !     up(3,i) = 0.100
+        ! else
+        !     up(1,i) = 1.000
+        !     up(2,i) = 0.000
+        !     up(3,i) = 1.000
+        ! end if
+
+        !! Shu-Osher
+        ! print*, i,x(i)
+        if (x(i) < -4.0 ) then
+            up(1,i) = 27./7.
+            up(2,i) = 4.*sqrt(35.)/7.0
+            up(3,i) = 31./3.
         else
-            up(1,i) = 1.000
-            up(2,i) = 0.000
-            up(3,i) = 1.000
+            up(1,i) = 1. + 0.2*sin(5*x(i))
+            up(2,i) = 0.0
+            up(3,i) = 1.0
         end if
     end do
 
